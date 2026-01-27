58b0aa1bdcb07e6a055430281f8eb290167c4fb5
ihexon <14349453+ihexon@users.noreply.github.com>
2026-01-27 22:10:11 +0800

    chore(ci): add dropbear build workflow

---
+++ build_dropbear.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+set -xe
+# Please build in alpine container using docker or podman
+apk add gcc git musl-dev zlib-dev make bash
+
+git clone https://github.com/mkj/dropbear.git -b DROPBEAR_2025.89
+
+cd dropbear
+LDFLAGS="-Wl,--gc-sections" CFLAGS="-ffunction-sections -fdata-sections" bash ./configure \
+    --disable-zlib \
+    --disable-syslog \
+    --disable-wtmpx \
+    --disable-pam \
+    --disable-utmpx \
+    --disable-wtmp \
+    --disable-shadow \
+    --disable-fuzz \
+    --enable-static
+make PROGRAMS="dropbear dbclient dropbearkey dropbearconvert scp" MULTI=1
+du -sk dropbearmulti
