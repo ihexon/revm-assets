bdb34612e85803b32911077a32d66992456c0ed6
ihexon <14349453+ihexon@users.noreply.github.com>
2026-01-27 19:21:09 +0800

    chore(ci): add build busybox workflow

--- build_busybox.sh
+++ build_busybox.sh
@@ -1,4 +1,17 @@
 #! /usr/bin/env bash
 set -xe
-wget https://busybox.net/downloads/busybox-1.37.0.tar.bz2 --output-document=- | tar -zxv -
-cd busybox-1.37.0 && make defconfig && make -j8 && make install
+
+mkdir -p out/tmp
+
+if [[ $(uname -m) == "aarch64" ]]; then
+    wget https://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/pool/main/b/busybox/busybox-static_1.37.0-4ubuntu1_arm64.deb --output-document=out/busybox.deb
+fi
+
+if [[ $(uname -m) == "x86_64" ]]; then
+    wget https://mirrors.tuna.tsinghua.edu.cn/ubuntu/pool/main/b/busybox/busybox-static_1.36.1-6ubuntu3.1_amd64.deb --output-document=out/busybox.deb
+fi
+
+dpkg -X out/busybox.deb out/tmp
+
+# run busybox for test
+./out/tmp/usr/bin/busybox
