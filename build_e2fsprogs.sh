646a1f4edd095d151731c26984ac83e4edbebf29
ihexon <14349453+ihexon@users.noreply.github.com>
2026-01-28 16:56:03 +0800

    feat(e2fsprogs): add e2fsprogs release

--- build_e2fsprogs.sh
+++ build_e2fsprogs.sh
@@ -12,9 +12,24 @@ export RELEASE_TAR="e2fsprogs-$PLT-$ARCH.tar.zst"
 
 git clone https://git.kernel.org/pub/scm/fs/ext2/e2fsprogs.git "$SRC_DIR" -b v1.47.3
 
-build_e2fsprogs_darwin(){
+build_e2fsprogs_darwin() {
     cd "$SRC_DIR"
-    ./configure --prefix="$PREFIX"
+    ./configure --prefix="$PREFIX" \
+        --with-udev-rules-dir="$PREFIX/etc/udev" \
+        --with-crond-dir=="$PREFIX/etc/crond" \
+        --enable-symlink-build \
+        --with-systemd-unit-dir="$PREFIX/etc/systemd"
+    make -j8
+    make install
+}
+
+build_e2fsprogs_linux() {
+    cd "$SRC_DIR"
+    ./configure --prefix="$PREFIX" \
+        --with-udev-rules-dir="$PREFIX/etc/udev" \
+        --with-crond-dir=="$PREFIX/etc/crond" \
+        --enable-symlink-build \
+        --with-systemd-unit-dir="$PREFIX/etc/systemd"
     make -j8
     make install
 }
@@ -24,5 +39,17 @@ release() {
     tar --zstd -cvf "$RELEASE_TAR" -C "$PREFIX" .
 }
 
-build_e2fsprogs_darwin
+build() {
+    cd "$WORKSPACE"
+
+    if [[ "$PLT" == "Linux" ]]; then
+        build_e2fsprogs_linux
+    fi
+
+    if [[ "$PLT" == "Darwin" ]]; then
+        build_e2fsprogs_darwin
+    fi
+}
+
+build
 release
