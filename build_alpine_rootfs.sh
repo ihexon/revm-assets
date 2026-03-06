f16976d1b78c44cb0d50d66ec972e5fe4296c46b
CAT <14349453+ihexon@users.noreply.github.com>
2026-03-07 05:08:05 +0800

    Add bsdtar and zstd to Alpine package installation

--- build_alpine_rootfs.sh
+++ build_alpine_rootfs.sh
@@ -13,7 +13,7 @@ export RELEASE_TAR="$PKG_NAME-$PLT-$ARCH.tar.zst"
 
 build_alpine_rootfs() {
     cd "$WORKSPACE"
-    docker run --name="$PKG_NAME" alpine:3.23.3 sh -c "apk update && apk add podman nftables bash"
+    docker run --name="$PKG_NAME" alpine:3.23.3 sh -c "apk update && apk add podman nftables bash bsdtar zstd"
 }
 
 release() {
