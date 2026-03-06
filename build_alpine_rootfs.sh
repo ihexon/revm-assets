1975799ed07cf67faa806d4b085998c8a797d882
CAT <14349453+ihexon@users.noreply.github.com>
2026-03-07 04:36:20 +0800

    add bash to Alpine root filesystem build

--- build_alpine_rootfs.sh
+++ build_alpine_rootfs.sh
@@ -13,7 +13,7 @@ export RELEASE_TAR="$PKG_NAME-$PLT-$ARCH.tar.zst"
 
 build_alpine_rootfs() {
     cd "$WORKSPACE"
-    docker run --name="$PKG_NAME" alpine:3.23.3 sh -c "apk update && apk add podman nftables"
+    docker run --name="$PKG_NAME" alpine:3.23.3 sh -c "apk update && apk add podman nftables bash"
 }
 
 release() {
