4ad6e505114de4d661dc84484516a8d367de95b9
ihexon <14349453+ihexon@users.noreply.github.com>
2026-05-14 12:09:30 +0800

    try to static link libkrun
    Signed-off-by: ihexon <14349453+ihexon@users.noreply.github.com>

--- build_libkrun.sh
+++ build_libkrun.sh
@@ -14,29 +14,42 @@ export commit_id="391409d0335d67ab3c7e86dcd16ea8af70f231a0"
 git clone https://github.com/ihexon/libkrun.git "$LIBKRUN_SRC"
 cd "$LIBKRUN_SRC" && git checkout "$commit_id"
 
+enable_libkrun_staticlib() {
+    cd "$LIBKRUN_SRC"
+    if ! grep -q '"staticlib"' src/libkrun/Cargo.toml; then
+        sed -i 's/crate-type = \["cdylib", "lib"\]/crate-type = ["cdylib", "staticlib", "lib"]/' src/libkrun/Cargo.toml
+    fi
+}
+
 build_libkrun_darwin() {
     brew tap slp/krun
     brew install virglrenderer lld
     brew info virglrenderer
 
     cd "$LIBKRUN_SRC"
+    enable_libkrun_staticlib
     make clean
     TIMESYNC=1 make PREFIX="$PREFIX" GPU=1 BLK=1 NET=1
 
     rm -rf "$PREFIX"
     TIMESYNC=1 make PREFIX="$PREFIX" GPU=1 BLK=1 NET=1 install
+    install -m 644 target/release/libkrun.a "$PREFIX/lib/"
 }
 
 build_libkrun_linux() {
     sudo apt update
     sudo apt install -y llvm clang libclang-dev libcap-ng-dev
 
+    export RUSTFLAGS="${RUSTFLAGS:-} -C linker=gcc -C link-arg=-static-libgcc"
+
     cd "$LIBKRUN_SRC"
+    enable_libkrun_staticlib
     make clean
     make PREFIX="$PREFIX" BLK=1 NET=1
 
     rm -rf "$PREFIX"
     make PREFIX="$PREFIX" BLK=1 NET=1 install
+    install -m 644 target/release/libkrun.a "$PREFIX/lib64/"
 }
 
 build_libkrun() {
